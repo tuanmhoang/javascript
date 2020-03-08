716af9af4d44a3e7b77da8246e6e6709d00f9b71
tuan <hoangminhtuan0505@gmail.com>
2020-03-08 09:47:30 +0700

    get the selected item index from ul

---
+++ getSelectedLi/script.js
@@ -0,0 +1,13 @@
+function getEventTarget(e) {
+    e = e || window.event;
+    return e.target || e.srcElement; 
+}
+
+var ul = document.getElementById('my-list');
+ul.onclick = function(event) {
+    let target = getEventTarget(event);
+    let li = target.closest('li'); // get reference
+    let nodes = Array.from( li.closest('ul').children ); // get array
+    let index = nodes.indexOf( li ); 
+    alert(index);
+};
